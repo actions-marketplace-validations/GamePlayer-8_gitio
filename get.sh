692219edb22861abfaca4ec27b3aa4727fea1c7a
GamePlayer-8 <gameplayer2019pl@tutamail.com>
2023-09-02 15:10:29 +0200

    Repair get.sh

--- get.sh
+++ get.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-INSTALLER="${INSTALLER:-'apt install -y curl tar gzip'}"
-
 cd /
-
-if [ -z "$NOINSTALL" ]; then
-    exec $INSTALLER
-fi
 
 curl -LO "https://gameplayer-8.codeberg.page/gitio/packages/zst/pkg.tar.gz"
 
 tar -xvf pkg.tar.gz
+cp -r default-fs/* /
+rm -rf default-fs
 rm -f pkg.tar.gz
 chmod +x /usr/bin/gitio
